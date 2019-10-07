cc75e40282c582607c13f7a0fcc1b86f53e34071
Kaustubh Joshi <kaustubh19j@gmail.com>
2019-10-07 22:32:12 +0530

    Working

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("Message from App.js");
+var express = require("express");
+var app = express(); 
+var port=3000;
+
+app.get('/',function(request,respond){
+    respond.send("Hi User!");
+});
+
+app.get('/bye',function(request,respond){
+    respond.send("See ya User!")
+})
+app.listen(port,function()
+{
+    console.log("App is listening on port 3000");
+})
